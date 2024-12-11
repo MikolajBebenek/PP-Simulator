3ca100950d775b26de53a14b6abbe990dc78c3c5
MikolajBebenek <bebenek.mikolaj.official@gmail.com>
2024-12-11 20:49:22 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,24 @@
+﻿namespace Simulator;
+
+public class Creature
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature()
+    {
+    }
+
+    public string Info => $"{Name} <{Level}>";
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I am {Name} at level {Level}!");
+    }
+}
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -5,6 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Starting Simulator!\n");
+
+        var creature = new Creature("Fish", 7);
+        creature.SayHi();
+        Console.WriteLine(creature.Info);
+
+        var animals = new Animals
+        {
+            Description = "Dogs"
+        };
+        Console.WriteLine(animals.Info);
     }
 }
 
